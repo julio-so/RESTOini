727677926e7abf3146d07e04ae929d6d4a19b15b
julio-so <62069814+julio-so@users.noreply.github.com>
2020-12-16 07:10:28 +0700

    Add files via upload

---
+++ data/Titik_1.js
@@ -0,0 +1,8 @@
+var json_Titik_1 = {
+"type": "FeatureCollection",
+"name": "Titik_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.427660858265583, -6.984112866596269 ] } }
+]
+}
